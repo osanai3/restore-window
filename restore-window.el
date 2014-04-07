e85cef5218b30ddba5f5dd544ea25b1a67a128b1
Koichi Osanai <osanai3@gmail.com>
2014-04-08 00:58:16 +0900

    initial commit

---
+++ restore-window.el
@@ -0,0 +1,28 @@
+;;; -*- coding: utf-8; lexical-binding: t -*-
+;;; restore-window.el --- restore killed window
+
+;; Copyright (C) 2014 by Koichi Osanai
+
+;; Author: Koichi Osanai <osanai3@gmail.com>
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'restore-window)
+
+;;; restore-window.el ends here
